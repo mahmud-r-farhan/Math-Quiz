52cb16737b109be38373dfe5539dc001ce525599
Mahmudur Rahman <114731414+mahmud-r-farhan@users.noreply.github.com>
2025-08-10 23:01:11 +0600

    CORS Policy :(

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,16 @@ const cors = require('cors');
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/quiz', quizRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/feedback', feedbackRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
